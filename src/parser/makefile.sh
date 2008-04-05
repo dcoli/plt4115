ffb86d58eeda594eaeda8c957355ebfbb603ae4b
davidbalbert <davidbalbert@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-05 21:33:30 +0000

    Added a working bash makefile.  TODO: Make a real makefile using make

--- src/parser/makefile.sh
+++ src/parser/makefile.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
+
 java JLex.Main scanner.lex
 mv scanner.lex.java Yylex.java
 java java_cup.Main < parser.cup
-javac -d . parser.java sym.java Yylex.java
-rm scanner.lex.java
 mv *.java ../syntax
