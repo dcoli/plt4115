b935b727d618ea8d052f1e6a0b592b106dfaa439
ma.danbo <ma.danbo@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-06 16:36:01 +0000

    entered javadoc comments to elucidate usage

--- trunk/src/settings/Action.java
+++ trunk/src/settings/Action.java
@@ -1,13 +1,18 @@
 /**
- * 
+ * Action is the container for the information we can save about an action.
+ * Right now this means the action's name and the arguments that the action
+ * can take. This shouldn't need to be manipulated directly, but by adding 
+ * items to the ActionTable, which handles creation of actions.
  */
 package settings;
 
 import java.util.List;
 
 /**
- * @author colin
- *
+ * @author dm2241
+ * @param	id	the name by which the action is called.
+ * @param	argList	the arguments that can be passed with the action.
+ * @see	ActionTable
  */
 public class Action {
 	private final String id;
@@ -18,6 +23,10 @@ public class Action {
 		this.argList = argList;
 	}	
 	
+/**
+ * 
+ * @return	a list of arguments, which is List type of Strings.
+ */
 	public List<String> getArgList () {
 /*		Iterator itr = this.argList.iterator();
 		System.out.println("getArgList args:");
@@ -30,6 +39,10 @@ public class Action {
 		return this.argList;
 	}
 	
+/**
+ * 
+ * @return the action's id.
+ */
 	public String getId(){
 		return this.id;
 	}
