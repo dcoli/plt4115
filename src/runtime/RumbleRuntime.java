ab4e2d980f563cdb4f8a573d81165eea0661ac62
davidbalbert <davidbalbert@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-06 02:59:02 +0000

    Added verbose mode

--- src/runtime/RumbleRuntime.java
+++ src/runtime/RumbleRuntime.java
@@ -1,3 +1,4 @@
+package Rumble;
 import Rumble.Environment;
 
 public class RumbleRuntime
@@ -5,8 +6,11 @@ public class RumbleRuntime
 	public static void main(String[] args) {
 		Environment e = new Environment();
 		
+		boolean verbose = (args[1] == "-v"); 
+		
 		while (!e.end()) {
 			e.step();
+			if (verbose) { System.out.println(e); }
 		}
 		System.out.println(e);		
 	}
