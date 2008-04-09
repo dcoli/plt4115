923d2ddedf236ae7dc04c8cc41fd623b7d665d73
jnankin <jnankin@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-09 02:16:59 +0000

    Changed grammar to move out BooleanExpression

--- src/parser/Makefile.bat
+++ src/parser/Makefile.bat
@@ -1,6 +1,5 @@
 java JLex.Main scanner.lex
 copy scanner.lex.java Yylex.java
 java java_cup.Main < parser.cup
-javac -d . parser.java sym.java Yylex.java
 del scanner.lex.java
 move *.java ../compiler/syntax
