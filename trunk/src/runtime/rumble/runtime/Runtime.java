9e2ad51ec1e50ad2ca4a9fda57d2da2d27cef369
davidbalbert <davidbalbert@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-15 00:01:50 +0000

    FIXED: bug in checking for "-v" flag

--- trunk/src/runtime/rumble/runtime/Runtime.java
+++ trunk/src/runtime/rumble/runtime/Runtime.java
@@ -7,11 +7,11 @@ public class Runtime
 	public static void main(String[] args) {
 		Environment e = new Environment();
 		
-		boolean verbose = (args.length > 0 && args[0] == "-v"); 
+		boolean verbose = (args.length > 0 && args[0].equals("-v"));		
 		
 		while (!e.end()) {
 			e.step();
-			if (verbose) { System.out.println(e); }
+			if (verbose && !e.end()) { System.out.println(e); }
 		}
 		System.out.println(e);	
 	}
