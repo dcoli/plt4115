135117578938df7136223a842a678342ce94818b
ma.danbo <ma.danbo@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-23 12:06:45 +0000

    create code

---
+++ trunk/src/codegeneration/Generator.java
@@ -0,0 +1,22 @@
+package codegeneration;
+
+import java.util.*;
+
+public class Generator {
+
+	Printer pr;
+	EnvironmentGenerator eg; 
+	String ENVNAME = "My Very First Environment";
+	
+	public Generator () {
+		pr = new Printer("Environment.java");
+	}
+	
+	protected void print(String text) {
+		pr.print(text);
+	}
+	
+	protected void closeFile() {
+		if (pr.close()) System.out.println("Generated file closed.");
+	}
+}
