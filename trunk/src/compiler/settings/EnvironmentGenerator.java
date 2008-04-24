f9afa76b021a72587224dc932071b2a27a8ec0b0
ma.danbo <ma.danbo@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-24 04:07:33 +0000

    more progress ... getting ready to use arraylist of rumVars

--- trunk/src/compiler/settings/EnvironmentGenerator.java
+++ trunk/src/compiler/settings/EnvironmentGenerator.java
@@ -1,36 +1,85 @@
 package compiler.settings;
 
+import java.util.ArrayList;
+
 public class EnvironmentGenerator extends Generator {
 
 	private String name;
+	private String CLS = "Environment";
+	private String PKG = "rumble.runtime";
 
-	/**
-	 * @param args
-	 */
-/*	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-*/		
-	public EnvironmentGenerator (String name) {
-		super(); 
+	public EnvironmentGenerator (String name, ArrayList rumVarsArray) {
 		this.name = name;
-		this.makeStandardVars();
-		this.makeRumVar("int", "days", "0");
-		super.closeFile();
+		makes();
+		closeFile();
 	}
 	
-	private void makeStandardVars () {
-		String str = "\tString NAME = \"My Very First Environment\"\n";
+	private void makes() {
+		makeHeader(PKG,CLS);
+		decStandardVars();
+		decRumVar("int", "days", "0");
+		makeConstructorHeader(CLS);
+		setStandardVar("num_steps","0");
+		print("// custom globals\n\n");
+		setRumVar("days","0");
+		makeRumVarGetAndSet("days", "int");
+		makeToString("days");
+		makeFooter();
+	}
+	
+	private void decStandardVars () {
+		String str = "\tString NAME = \"" + this.name + "\"\n";
 		str += "\tprivate int num_steps";
 		str += ";\n\n";
-		super.print(str);
+		print(str);
+	}
+
+	private void setStandardVar(String name, String value) {
+		print("\tthis."+name+" = " + value +";+n");
 	}
 	
-	private void makeRumVar (String type, String name, String value) {
+	private void decRumVar (String type, String name, String value) {
 		String str = "\tprivate "+type+" rumVar_"+name;
 		if ( value != null ) str += " = " + value;
 		str += ";\n\n";
-		super.print(str);
+		print(str);
 	}
 
-}
+	private void setRumVar (String name, String value) {
+		String str = "\tthis.rumVar_"+name+" = "+value;
+		str += ";\n\n";
+		print(str);
+	}
+
+	private void makeRumVarGetAndSet(String name, String type) {
+		String str = "public "+type+" getRumVar_"+name+"() {\n";
+		str += "\treturn this.rumVar_"+name+";\n}\n\n";
+		
+		str += "private boolean setRumVar_"+name+"("+type+" "+name+") {\n";
+		str += "\tthis.rumVar_"+name+" = "+name+";\n";
+		str += "\treturn true;\n";
+		str += "}\n\n";
+		
+		print(str);
+	}
+	
+	private void makeToString(String name) {
+		String str = "public String toString() {\n";
+		str += "\treturn \"{\\n\\tenvironment : \\\"\" + this.NAME + \"\\\",\\n\\t"+name+" : \" + this.rumVar_days + \",\\n}\\n\";\n";
+		str += "}";
+		print(str);
+	}
+
+/*	private void makeStep(ArrayList rumVarsArray)
+	public void step() {
+		// standard rumble stuff
+		this.num_steps++;
+		
+		// everything else
+		this.setRumVar_days(this.getRumVar_days() + 1);
+	}
+	
+	public boolean end() {
+		return this.getRumVar_days() == 10;
+	}
+*/}
