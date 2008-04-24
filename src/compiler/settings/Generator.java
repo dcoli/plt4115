dd34aa27a44e31f483e526d51a31e1ae0ebcc82d
ma.danbo <ma.danbo@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-24 04:07:33 +0000

    more progress ... getting ready to use arraylist of rumVars

--- src/compiler/settings/Generator.java
+++ src/compiler/settings/Generator.java
@@ -19,4 +19,31 @@ public class Generator {
 	protected void closeFile() {
 		if (pr.close()) System.out.println("Generated file closed.");
 	}
+
+	protected void makeHeader(String pkg, String cls) {
+		print("package "+pkg+";\n\n");
+		print("public class "+cls+"\n\n");
+		print("{\n");
+	}
+
+	protected void makeFooter() {
+		print("}\n");
+	}
+
+	protected void makeConstructorHeader(String cls) {
+		print(""+cls + "() {\n");
+		print("// standard rumble stuff\n");
+	}			
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Array varArray = {"int","days","0"};
+		ArrayList rumVarsArray = new ArrayList(3);
+//		rumVarsArray.add("int","days","0");
+		EnvironmentGenerator eg = new EnvironmentGenerator("hello",rumVarsArray);
+	}
+
 }
