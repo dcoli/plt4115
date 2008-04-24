ea6ad93d3ba008fd1efaa52f256bd3b38a022be2
jnankin <jnankin@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-04-24 13:40:00 +0000

    Very simple codegenerator working

--- src/compiler/syntax/ASTNode.java
+++ src/compiler/syntax/ASTNode.java
@@ -4,22 +4,28 @@ import java.util.LinkedList;
 
 public class ASTNode {
 	private Object descriptor;
-	private LinkedList opList;
+	private Object[] opList;
+	private int lineNumber;
+	private int opCount = 0;
 	
 	public ASTNode (Object descriptor){
 		this.descriptor = descriptor;
-		opList = new LinkedList();
+		opList = new Object[4];
 	}
 	
 	public void pushOp(Object o){
-		opList.addFirst(o);
+		opList[opCount++] = o;
 	}
 	
 	public Object getOp(int i){
-		return opList.get(i);
+		return opList[i];
 	}
 	
 	public Object getDescriptor(){
 		return descriptor;
+	}
+	
+	public void setLineNumber(int i){
+		lineNumber = i;
 	}
 }
