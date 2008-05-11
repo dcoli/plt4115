b655d1a49d831503ec0dda645d75e01881717813
nicholasbs <nicholasbs@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-05-11 06:04:39 +0000

    added class printout to toString()

--- trunk/src/compiler/syntax/ASTNode.java
+++ trunk/src/compiler/syntax/ASTNode.java
@@ -4,64 +4,63 @@ import java.util.LinkedList;
 
 public class ASTNode {
 	private Object descriptor;
-	//private LinkedList<Object> opList;
+	// private LinkedList<Object> opList;
 	private Object[] opList;
 	private int lineNumber;
 	private int numOps = 0;
-	
-	public ASTNode (Object descriptor){
+
+	public ASTNode(Object descriptor) {
 		this.descriptor = descriptor;
-		//opList = new LinkedList<Object>();
-		
-		// did this for debugging purposes.  talk to me b4 changing back.
+		// opList = new LinkedList<Object>();
+
+		// did this for debugging purposes. talk to me b4 changing back.
 		opList = new Object[4];
 	}
-	
-	public void pushOp(Object o){
+
+	public void pushOp(Object o) {
 		opList[numOps++] = o;
-		//opList.add(o);
+		// opList.add(o);
 	}
-	
-	public Object getOp(int i){
+
+	public Object getOp(int i) {
 		Object o;
-		
+
 		try {
 			return opList[i];
-		}
-		catch (IndexOutOfBoundsException e) {
+		} catch (IndexOutOfBoundsException e) {
 			o = null;
 		}
-		
+
 		return o;
 	}
-	
-	public Object getDescriptor(){
+
+	public Object getDescriptor() {
 		return descriptor;
 	}
-	
-	public void setLineNumber(int i){
+
+	public void setLineNumber(int i) {
 		this.lineNumber = i;
 	}
-	
+
 	public int getLineNumber() {
 		return this.lineNumber;
 	}
-	
-	public int getNumberOfOperands(){
+
+	public int getNumberOfOperands() {
 		return numOps;
 	}
-	
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("ASTNode: Descriptor = " + descriptor.toString());
 		sb.append("\nOpList:");
 		for (int i = 0; i < 4; i++) {
 			Object o = opList[i];
 			if (o != null)
-				sb.append("\n" + o.toString());
+				sb.append("\n" + o.getClass() + ": "+ o.toString());
 		}
-		
+
 		return sb.toString();
 	}
 }
