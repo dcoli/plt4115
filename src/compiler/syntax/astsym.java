088d47d8836fc5ebe595d0d346eeed4d8672e25d
nicholasbs <nicholasbs@0f0cb93e-f944-0410-8cd1-83fb6cdaa90b>
2008-05-11 06:05:14 +0000

    added SYSTEM_PART_REF_ONLY so that e.g., $part[0] will work

--- src/compiler/syntax/astsym.java
+++ src/compiler/syntax/astsym.java
@@ -1,18 +1,18 @@
 package compiler.syntax;
 
 public class astsym {
-  public static final int FUNCTION_CALL = 103;
-  public static final int STEP_CALL = 104;
-  public static final int SYSTEM_VAR = 105;
-  public static final int SYSTEM_VAR_NAME = 106;
-  public static final int SYSTEM_PART_REF = 107;
-  public static final int SYSTEM_GLOBAL = 108;
-  public static final int CONSTRAINT = 109;
-  public static final int DECLARATION = 110;
-  public static final int IDLIST = 111;
-  public static final int BLOCK = 112;
-  public static final int ACTIONS = 113;
-  public static final int ARG = 114;
-  public static final int ARGLIST = 115;
+	public static final int FUNCTION_CALL = 103;
+	public static final int STEP_CALL = 104;
+	public static final int SYSTEM_VAR = 105;
+	public static final int SYSTEM_VAR_NAME = 106;
+	public static final int SYSTEM_PART_REF = 107;
+	public static final int SYSTEM_GLOBAL = 108;
+	public static final int CONSTRAINT = 109;
+	public static final int DECLARATION = 110;
+	public static final int IDLIST = 111;
+	public static final int BLOCK = 112;
+	public static final int ACTIONS = 113;
+	public static final int ARG = 114;
+	public static final int ARGLIST = 115;
+	public static final int SYSTEM_PART_REF_ONLY = 116;
 }
-
